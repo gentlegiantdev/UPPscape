6a58d3537db661fc2257e58709e72b49760959f5
Eric <gentlegiantdev@gmail.com>
2022-10-02 14:57:42 -0700

    chore: added comments and updated readme

--- controllers/posts.js
+++ controllers/posts.js
@@ -48,27 +48,7 @@ module.exports = {
       });
       console.log("Post has been added!");
       console.log(req.body);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
       res.redirect(`/feed/${req.body.account}`);
-=======
-      res.redirect("/accounts");
->>>>>>> parent of 6658a0b (chore: final styling for mvp)
-=======
-      res.redirect("/accounts");
->>>>>>> parent of 6658a0b (chore: final styling for mvp)
-=======
-      res.redirect("/accounts");
->>>>>>> parent of 6658a0b (chore: final styling for mvp)
-=======
-      res.redirect("/accounts");
->>>>>>> parent of 6658a0b (chore: final styling for mvp)
-=======
-      res.redirect("/accounts");
->>>>>>> parent of 6658a0b (chore: final styling for mvp)
     } catch (err) {
       console.log(err);
     }
