f17636e2df1b62acb111d54c9dc5264b8e0035dd
Eric <gentlegiantdev@gmail.com>
2022-10-11 11:52:32 -0700

    feat: added about, tos, privacy and cookie policies

--- controllers/home.js
+++ controllers/home.js
@@ -2,4 +2,17 @@ module.exports = {
   getIndex: (req, res) => {
     res.render("index.ejs");
   },
+  getAbout: (req, res) => {
+    res.render("about.ejs");
+  },
+  getPrivacy: (req, res) => {
+    res.render("privacypolicy.ejs");
+  },
+  getCookie: (req, res) => {
+    res.render("privacypolicy.ejs");
+  },
+  getTerms: (req, res) => {
+    res.render("termsofuse.ejs");
+  },
+
 };
--- routes/main.js
+++ routes/main.js
@@ -8,6 +8,10 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
+router.get("/about", homeController.getAbout);
+router.get("/privacypolicy", homeController.getPrivacy);
+router.get("/privacypolicy/#cookies", homeController.getCookie);
+router.get("/termsofuse", homeController.getTerms);
 router.get("/accounts", ensureAuth, accountsController.getAccounts);
 router.get("/profile/", ensureAuth, postsController.getProfile);
 router.get("/feed/:id", ensureAuth, postsController.getFeed);
